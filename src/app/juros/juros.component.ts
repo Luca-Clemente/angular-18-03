7eb5798d754565043903fef18856eaf1d48d8bd6
Luca-Clemente <lukpinkman@hotmail.com>
2022-03-18 19:51:44 -0300

    first commit

---
+++ src/app/juros/juros.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros',
+  templateUrl: './juros.component.html',
+  styleUrls: ['./juros.component.css'],
+})
+export class JurosComponent implements OnInit {
+  vp: number;
+  j: number;
+  n: number;
+
+  constructor() {
+    this.vp = 100;
+    this.j = 2.5;
+    this.n = 3;
+  }
+
+  ngOnInit() {}
+}
